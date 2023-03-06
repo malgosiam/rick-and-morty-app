c63b877a91a14a6aff3b843f7882aa0a9744e367
Kevin Weyrowitz <kevinweyrowitz93@gmail.com>
2023-03-06 16:10:31 +0100

    refactor searchButton

---
+++ components/search-bar/search-bar.js
@@ -0,0 +1,35 @@
+export function createSearchBar(onSubmit) {
+  const form = document.createElement("form");
+  const input = document.createElement("input");
+  const searchButton = document.createElement("button");
+  const searchImage = document.createElement("img");
+
+  input.setAttribute("name", "query");
+  input.setAttribute("type", "text");
+  input.setAttribute("placeholder", "search characters");
+  input.setAttribute("aria-label", "character name");
+  searchButton.setAttribute("type", "submit");
+  searchButton.setAttribute("aria-label", "search for character");
+  searchImage.setAttribute("src", "assets/magnifying-glass.png");
+  searchImage.setAttribute("alt", "searchIcon");
+
+  searchButton.classList.add("search-bar__button");
+  searchImage.classList.add("search-bar__icon");
+  input.classList.add("search-bar__input");
+  form.classList.add("search-bar");
+  searchButton.append(searchImage);
+  form.append(input, searchButton);
+
+  form.addEventListener("submit", onSubmit);
+
+  return form;
+}
+
+/*function submit(event, query) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  query = data.query;
+  fetchCharacters();
+}*/
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import { createCharacterCard } from "./components/card/card.js";
 import { prevPage, nextPage } from "./components/nav-button/nav-button.js";
-
+import { createSearchBar } from "./components/search-bar/search-bar.js";
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
-const searchBar = document.querySelector('[data-js="search-bar"]');
+const form = createSearchBar((event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  searchQuery = data.query;
+  fetchCharacters();
+});
+searchBarContainer.append(form);
+/* const searchBar = document.querySelector('[data-js="search-bar"]'); */
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
@@ -51,7 +60,7 @@ nextButton.addEventListener("click", async () => {
   await fetchCharacters();
 });
 
-searchBar.addEventListener("submit", (event) => {
+/* searchBar.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const formData = new FormData(event.target);
@@ -59,5 +68,5 @@ searchBar.addEventListener("submit", (event) => {
   searchQuery = data.query;
   fetchCharacters();
 });
-
+ */
 await fetchCharacters();
